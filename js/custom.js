61e6b5b76ab9aa432ed8f3704af26fa3d9593a31
aamir <aamir.ramay1@gmail.com>
2017-06-18 15:35:02 +0500

    TutorHub Web Application Landing Page

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+// drop down 
+$('.ui.dropdown').dropdown();
+
+// sidebar 
+$('.ui.sidebar').sidebar('attach events','.toc.item');
+
+//local location variable and feeback
+/*
+var location = [{
+	title:'Physics',
+	description:'9Class'
+},
+
+{
+	title:'Chemistry',
+	description:'9Class'
+
+},
+
+{
+	title:'Biology',
+	description:'9Class'
+
+},
+
+{
+    title:'Boston',
+	description:'Aamir'
+},];
+
+$('.ui.search.location').search({
+ 
+ source:location,
+ searchFields:['title','description'],
+ searchFullText:false
+
+});
+*/
